1dae705bc01314cfcb737467b1a09507e0a8c213
Septicake <septicake056@gmail.com>
2025-09-25 12:09:07 -0400

    Add name verification

--- src/input/handlers/name.cpp
+++ src/input/handlers/name.cpp
@@ -1,20 +1,28 @@
 #include "characters/player.hpp"
 #include "input/handlers/handler.hpp"
+#include "parsers/parsers.hpp"
 #include "utils.hpp"
 #include <input/handlers/name.hpp>
 #include <input/handlers/commands.hpp>
-#include <iostream>
+#include <lexy/action/match.hpp>
+#include <lexy/input/string_input.hpp>
 #include <memory>
 
 bool NameHandler::process(std::shared_ptr<Player> player, std::string input) {
     std::string trimmed = trim(input);
+    if(trimmed.empty()) {
+        player->sendMsg("Name cannot be blank\r\nName:");
+        return false;
+    }
+    if(!lexy::match<CharacterParser>(lexy::zstring_input(("@"+trimmed).c_str()))) {
+        player->sendMsg("Name must only consist of [A-Z], [a-z], underscore, and hyphen\r\nName:");
+        return false;
+    }
     player->name = trimmed;
-    std::cout << trimmed << " has been chosen as the name" << std::endl;
     next(player);
     return true;
 }
 
 void NameHandler::next(std::shared_ptr<Player> player) {
-    std::cout << "setting handler" << std::endl;
     player->handler = std::static_pointer_cast<Handler>(std::make_shared<CommandHandler>());
 }
