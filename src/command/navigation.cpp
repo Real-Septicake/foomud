1940146905886cefe02cf7d6110af13768708bcf
Septicake <septicake056@gmail.com>
2025-09-26 20:15:50 -0400

    different responses based on arguments passed to go command

--- src/command/navigation.cpp
+++ src/command/navigation.cpp
@@ -16,6 +16,10 @@ bool commands::go(std::shared_ptr<Character> c, Arguments &args) {
         c->sendMsg("Go where?\r\n");
         return false;
     }
+    if(args.size() != 1) {
+        c->sendMsg("You can only go one way\r\n");
+        return false;
+    }
     std::string d = args[0];
     auto dir = parseDirection(d);
     if(dir == Direction::None) {
@@ -24,7 +28,17 @@ bool commands::go(std::shared_ptr<Character> c, Arguments &args) {
     }
     auto e = c->current_room->exits.find(dir);
     if(e == c->current_room->exits.end()) {
-        c->sendMsg("Into that wall?\r\n");
+        switch(dir) {
+            case Direction::Up:
+                c->sendMsg("The ceiling? How?\r\n");
+                break;
+            case Direction::Down:
+                c->sendMsg("No digging straight down\r\n");
+                break;
+            default:
+                c->sendMsg("Into that wall?\r\n");
+                break;
+        }
         return false;
     }
     c->current_room->send(c->name + " has gone " + std::string(enchantum::to_string(dir)) + "\r\n", {c});
